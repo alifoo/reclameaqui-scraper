6d13f93d5e54e65673275d979fc21049becf6164
alifoo <alissonayres.trb@gmail.com>
2025-09-21 23:20:45 -0300

    now fetching detailed page

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import time
 import random
+import platform
 import pandas as pd
 from playwright.sync_api import sync_playwright, TimeoutError, expect
 from playwright_stealth import Stealth
@@ -16,9 +17,104 @@ def check_cookie(page):
         print("No cookie banner found or it was already dismissed.")
 
 
-def scrape_first_page_complaints(company_name: str, pages_to_scrape: int):
+def fetch_complaint_info(soup, i):
+    complaint_data = {}
+    print(f"Opened complaint detail page for complaint {i + 1}")
+
+    def safe_get_text(element):
+        return element.get_text(strip=True) if element else "Not found"
+
+    complaint_container = soup.select_one(".sc-98c0be-3.fmbfWT")
+
+    if complaint_container:
+        print("Complaint container found, extracting details...")
+
+        complaint_data["location"] = safe_get_text(
+            complaint_container.select_one("[data-testid='complaint-location']")
+        )
+        complaint_data["detailed_date"] = safe_get_text(
+            complaint_container.select_one("[data-testid='complaint-creation-date']")
+        )
+        complaint_data["full_title"] = safe_get_text(
+            soup.select_one("[data-testid='complaint-title']")
+        )
+        complaint_data["full_description"] = safe_get_text(
+            soup.select_one("p[data-testid='complaint-description']")
+        )
+
+        response_header = soup.find("div", type="ANSWER")
+        complaint_data["company_response"] = (
+            safe_get_text(response_header.find_next_sibling("p"))
+            if response_header
+            else "Not found"
+        )
+
+        consideration_header = soup.find("div", type="FINAL_ANSWER")
+        complaint_data["final_consideration"] = (
+            safe_get_text(consideration_header.find_next_sibling("p"))
+            if consideration_header
+            else "Not found"
+        )
+
+        evaluation_panel = soup.select_one(
+            "div[data-testid='complaint-evaluation-interaction']"
+        )
+        if evaluation_panel:
+            complaint_data["evaluation_solved"] = safe_get_text(
+                evaluation_panel.select_one("div[data-testid='complaint-status']")
+            )
+            deal_again_header = evaluation_panel.find(
+                "span", string="Voltaria a fazer negócio?"
+            )
+            complaint_data["deal_again"] = (
+                safe_get_text(deal_again_header.find_next_sibling("div").find("div"))
+                if deal_again_header
+                else "Not found"
+            )
+
+            score_header = evaluation_panel.find("span", string="Nota do atendimento")
+            complaint_data["score"] = (
+                safe_get_text(score_header.find_next_sibling("div").find("div"))
+                if score_header
+                else "Not found"
+            )
+        print("Details extracted successfully.")
+    else:
+        location, detailed_date, full_title, full_description = (
+            "Container not found",
+            "Container not found",
+            "Container not found",
+            "Container not found",
+        )
+        company_response, evaluation_final_consideration = (
+            "Container not found",
+            "Container not found",
+        )
+        evaluation_solved, evaluation_deal_again, evaluation_score = (
+            "Container not found",
+            "Container not found",
+            "Container not found",
+        )
+        complaint_data = {
+            "location": location,
+            "detailed_date": detailed_date,
+            "full_title": full_title,
+            "full_description": full_description,
+            "company_response": company_response,
+            "evaluation_final_consideration": evaluation_final_consideration,
+            "evaluation_solved": evaluation_solved,
+            "evaluation_deal_again": evaluation_deal_again,
+            "score": evaluation_score,
+        }
+
+    return complaint_data
+
+
+def scrape_complaints(company_name: str, pages_to_scrape: int):
     print(f"Starting to scrape {company_name}...")
-    url = f"https://www.reclameaqui.com.br/empresa/{company_name}/lista-reclamacoes/"
+    base_url = "https://www.reclameaqui.com.br"
+    start_url = f"{base_url}/empresa/{company_name}/lista-reclamacoes/?status=EVALUATED"
+    url = f"https://www.reclameaqui.com.br/empresa/{company_name}/lista-reclamacoes/?status=EVALUATED"
     all_complaints_data = []
 
     with Stealth().use_sync(sync_playwright()) as p:
@@ -27,18 +123,18 @@ def scrape_first_page_complaints(company_name: str, pages_to_scrape: int):
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
             viewport={"width": 1920, "height": 1080},
         )
-        page = context.new_page()
+        main_page = context.new_page()
 
         print(f"Navigating to {url}...")
-        page.goto(url, wait_until="domcontentloaded", timeout=60000)
+        main_page.goto(url, wait_until="domcontentloaded", timeout=60000)
 
-        check_cookie(page)
+        check_cookie(main_page)
 
         for page_num in range(1, pages_to_scrape + 1):
             print(f"\n--- Scraping Page {page_num} of {pages_to_scrape} ---")
 
             try:
-                if "verify-human" in page.url:
+                if "verify-human" in main_page.url:
                     print("CAPTCHA detected, stopping.")
                     break
 
@@ -46,43 +142,99 @@ def scrape_first_page_complaints(company_name: str, pages_to_scrape: int):
                 print(
                     f"Waiting for complaint list to load using selector: {complaint_list_selector}"
                 )
-                page.wait_for_selector(complaint_list_selector, timeout=30000)
-                # time.sleep(3)
 
-                first_title_element = page.locator("h4.sc-1pe7b5t-1.bVKmkO").first
+                try:
+                    print(f"Waiting for complaint list: {complaint_list_selector}")
+                    main_page.wait_for_selector(complaint_list_selector, timeout=30000)
+                    print("Complaint list loaded.")
+                except TimeoutError:
+                    print("Timeout while waiting for complaint list")
+                    continue
+
+                first_title_element = main_page.locator("h4.sc-1pe7b5t-1.bVKmkO").first
                 old_title_text = first_title_element.inner_text()
                 print(f"First title on this page '{old_title_text}'")
 
-                html_content = page.content()
+                html_content = main_page.content()
                 soup = BeautifulSoup(html_content, "html.parser")
                 complaint_containers = soup.select("div.sc-1pe7b5t-0.eJgBOc")
                 if not complaint_containers:
-                    print("no complaint containers found.")
+                    print("No complaint containers found on this page.")
+                    print(main_page.content()[:500])
                     break
 
-                complaints_data = []
+                page_links = []
                 for complaint in complaint_containers:
-                    title = complaint.find("h4", class_="sc-1pe7b5t-1 bVKmkO").get_text(
-                        strip=True
+                    title_preview_tag = complaint.find(
+                        "h4", class_="sc-1pe7b5t-1 bVKmkO"
                     )
-                    description = complaint.find(
-                        "p", class_="sc-1pe7b5t-2 eHoNfA"
-                    ).get_text(strip=True)
-                    date = complaint.find(
-                        "span", class_="sc-1pe7b5t-5 dspDoZ"
-                    ).get_text(strip=True)
-                    complaints_data.append(
-                        {"title": title, "description": description, "date": date}
+                    title_preview = (
+                        title_preview_tag.get_text(strip=True)
+                        if title_preview_tag
+                        else "No title"
                     )
 
-                all_complaints_data.extend(complaints_data)
-                print(f"Scraped {len(complaints_data)} complaints from this page.")
+                    description_preview_tag = complaint.find(
+                        "p", class_="sc-1pe7b5t-2 eHoNfA"
+                    )
+                    description_preview = (
+                        description_preview_tag.get_text(strip=True)
+                        if description_preview_tag
+                        else "No description"
+                    )
+
+                    date_preview_tag = complaint.find(
+                        "span", class_="sc-1pe7b5t-5 dspDoZ"
+                    )
+                    date_preview = (
+                        date_preview_tag.get_text(strip=True)
+                        if date_preview_tag
+                        else "No date"
+                    )
+
+                    link_tag = complaint.find("a", id="site_bp_lista_ler_reclamacao")
+
+                    if (
+                        title_preview
+                        and description_preview
+                        and date_preview
+                        and link_tag
+                    ):
+                        print(f"Found complaint: {title_preview}")
+                        page_links.append(
+                            {
+                                "url": base_url + link_tag["href"],
+                                "title_preview": title_preview,
+                                "desc_preview": description_preview,
+                                "date_preview": date_preview,
+                            }
+                        )
+
+                for i, link_info in enumerate(page_links):
+                    print(f"  -> Navigating to complaint {i+1}: {link_info['url']}")
+                    main_page.goto(link_info["url"], wait_until="domcontentloaded")
+                    time.sleep(random.uniform(2, 4))
+
+                    detail_soup = BeautifulSoup(main_page.content(), "html.parser")
+                    details = fetch_complaint_info(detail_soup, i)
+
+                    link_info.update(details)
+                    all_complaints_data.append(link_info)
+
+                    print("  <- Navigating back to the list page...")
+                    main_page.go_back(wait_until="domcontentloaded")
+                    time.sleep(random.uniform(5, 10))
+
+                print(f"Finished scraping details for page {page_num}.")
+                time.sleep(random.uniform(4, 6))
 
                 if page_num == pages_to_scrape:
                     print("reached limit. stopping.")
                     break
 
-                next_page_button = page.get_by_test_id("next-page-navigation-button")
+                next_page_button = main_page.get_by_test_id(
+                    "next-page-navigation-button"
+                )
                 if next_page_button.is_visible():
                     print("Moving to next page...")
                     next_page_button.hover()
@@ -95,7 +247,7 @@ def scrape_first_page_complaints(company_name: str, pages_to_scrape: int):
                     )
                     print("page updated")
 
-                    sleep_time = random.uniform(5, 12)
+                    sleep_time = random.uniform(4, 6)
                     print(f"waiting for {sleep_time:.2f} seconds before next page...")
                     time.sleep(sleep_time)
                 else:
@@ -117,7 +269,7 @@ def scrape_first_page_complaints(company_name: str, pages_to_scrape: int):
 
 if __name__ == "__main__":
     company_name = "mattel-do-brasil-fisher-price-barbie-hotwheels-polly-monster-high"
-    scraped_data = scrape_first_page_complaints(company_name, 50)
+    scraped_data = scrape_complaints(company_name, 50)
 
     if scraped_data:
         df = pd.DataFrame(scraped_data)
